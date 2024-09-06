29ac0636e3d9c20f6ddf6a5667975a600f6131bc
alisaleks <alisa.aleksanyan@alumni.esade.edu>
2024-09-06 13:17:23 +0200

    Initial commit to new repository

---
+++ update_dataset.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\aaleksan\OneDrive - Amplifon S.p.A\Documentos\python_alisa\MBreport"
+git add mbreport_query_new.xlsx
+git commit -m "Automatic dataset update"
+git push origin main
+exit /b 0
